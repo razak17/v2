674c301ba04148666bb61bb637e2f34916412459
Abdul Razak <moabdulrazak17@gmail.com>
2025-08-21 17:43:08 +0000

    chore: initial site setup

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "razak",
+  description: "Personal website of razak, built with Next.js.",
+  url: "https://v2.razakmo.tech",
+  ogImage: "https://v2.razakmo.tech/opengraph-image.png",
+  links: {
+    x: "https://twitter.com/theRazakMo",
+    github: "https://github.com/razak17/v2",
+    githubProfile: "https://github.com/razak17",
+  },
+};
